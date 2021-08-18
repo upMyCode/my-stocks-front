5971d8e37ba86f8de6f2459d1dfea5af05c8edb7
Maverick17user <maksim.epifanov.2000@mail.ru>
2021-08-18 17:58:13 +0300

    review fix

--- src/components/MutateStockModal.jsx
+++ src/components/MutateStockModal.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { Box, FormControl, InputLabel, Input } from '@material-ui/core';
+import { 
+  Box, 
+  Button,
+  FormControl, 
+  InputLabel, 
+  Input,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 
 export default function MutateStockModal({ 
   isOpen = false, 
@@ -36,7 +41,6 @@ export default function MutateStockModal({
   const handleChangCost = (e) => {
     setNewStockCost(e.target.value)
   }
-
 
   return (
     <Box>
