ad2293427dae2daf8a5d99cdcadbc19500ad72b0
Maverick17user <maksim.epifanov.2000@mail.ru>
2021-08-18 19:15:53 +0300

    request fixes

--- src/components/AddStockButton.jsx
+++ src/components/AddStockButton.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { IconButton } from '@material-ui/core'
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import MutateStockModal from './MutateStockModal';
+import transformStrNumToFloat from '../helpers/transformStrNumToFloat';
 
 export default function AddStockButton({
   refetch,
@@ -21,7 +22,7 @@ export default function AddStockButton({
       variables: {
         input: {
           ticker: newStockTicker,
-          cost: newStockCost,
+          cost: transformStrNumToFloat(newStockCost),
         }
       }
     }).then(refetch)
---
+++ src/helpers/transformStrNumToFloat.js
@@ -0,0 +1,5 @@
+const transformStrNumToFloat = (strValue) => {
+    return parseFloat(strValue)
+}
+
+export default transformStrNumToFloat;
